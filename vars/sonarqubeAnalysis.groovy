d67a763dbf608a438669beffbdad5503438a0ce7
prameela <prameelakatreddy@gmail.com>
2023-12-30 16:59:12 +0530

    clean and git checkout

---
+++ vars/sonarqubeAnalysis.groovy
@@ -0,0 +1,6 @@
+def call() {
+    withSonarQubeEnv('sonar-server') {
+                    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=youtube -Dsonar.projectKey=youtube '''
+                }
+    }
+}
